125fdf88df45d0ad1fa64efd728b5144e0602afb
tonyreina <tonyreina@local>
2018-08-04 21:54:07 +0000

    Lab 5

--- lab5/tasks/train_lstm_line_predictor_on_iam.sh
+++ lab5/tasks/train_lstm_line_predictor_on_iam.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-pipenv run python training/run_experiment.py --save '{"dataset": "IamLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc"}'
+pipenv run python training/run_experiment.py '{"dataset": "IamLinesDataset", "model": "LineModelCtc", "network": "line_lstm_ctc"}'
